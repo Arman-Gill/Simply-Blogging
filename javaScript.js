ac0d8fc317f6420b14cc3735fc9096350dc6cffc
Arman-Gill <52617758+Arman-Gill@users.noreply.github.com>
2022-04-06 11:27:36 +0530

    final changes

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,17 @@
+
 const openModalLink = document.querySelector('[data-modal-target]')
 const closeModalButton = document.querySelector('[data-close-button]')
+const allInDevelopmentLinks = document.querySelectorAll('a')
+
+allInDevelopmentLinks.forEach(link =>{
+
+    if (link != openModalLink){
+        link.addEventListener('click', ()=>{
+            alert('Sorry! This page or utility is under development.')
+        })
+    
+    }
+})
 
 openModalLink.addEventListener('click', () => {
 const modal = document.querySelector(openModalLink.dataset.modalTarget)
